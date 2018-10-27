6460d8d05c0fa57e4ee72e3d3558e2c8d2b265ca
ciec-20171103189 <1103385493@qq.com>
2018-10-27 10:07:54 +0800

    Initial Commit

---
+++ ciec-plan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ciec-plan
+//
+//  Created by s20171103189 on 2018/10/27.
+//  Copyright © 2018 s20171103189. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
